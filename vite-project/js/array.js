2e3e1dddfa1804ac866c097c1c2e8211df54ddc1
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-10-27 10:18:40 -0400

    started

---
+++ vite-project/js/array.js
@@ -0,0 +1,58 @@
+const inventory = [
+    {
+        item: "Computer",
+        quantity: 1,
+        price: 1000,
+        in_stock: true,
+        type: ["electronic"],
+    },
+    {
+        item: "Phone",
+        quantity: 1,
+        price: 800,
+        in_stock: false,
+        type: ["electronic"],
+    },
+    {
+        item: "Tablet",
+        quantity: 1,
+        price: 900,
+        in_stock: true,
+        type: ["electronic"],
+    },
+    {
+        item: "Headphones",
+        quantity: 1,
+        price: 300,
+        in_stock: true,
+        type: ["electronic"],
+    },
+    {
+        item: "Pencils",
+        quantity: 10,
+        price: 5,
+        in_stock: true,
+        type: ["craft", "school"],
+    },
+    {
+        item: "Markers",
+        quantity: 8,
+        price: 6,
+        in_stock: true,
+        type: ["school", "craft"],
+    },
+    {
+        item: "Folders",
+        quantity: 2,
+        price: 4,
+        in_stock: true,
+        type: ["school"],
+    },
+    {
+        item: "Binder",
+        quantity: 1,
+        price: 5,
+        in_stock: true,
+        type: ["school"],
+    },
+]
