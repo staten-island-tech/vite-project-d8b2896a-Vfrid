2330fb1087bcc4bbcd95dc16bf247ab1c2c00000
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-11-01 10:20:27 -0400

    fdsaf

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,8 +2,30 @@ import { inventory } from './array.js'
 
 const DOMSelectors ={
   app: document.querySelector('#app'),
+  checkbox_form: document.querySelector('#checkbox_form'),
+  checkbox: document.querySelector('#checkbox'),
 };
-console.log(inventory)
+const cheap = inventory.filter((invent)=>invent.price<100);
+
+function createcheapcard(){
+  return cheap.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
+    "afterbegin", 
+`<div class="box">
+    <div class="item">
+      <div class="item_link">
+        <div class="item_bg"></div>
+
+        <div class="item_title">
+          <p class="name_title">${thing.item} </p>
+        </div>
+          <span class="item_date"> $${thing.price}
+          </span>
+      </div>
+    </div>
+  </div>`
+  ))
+  }
+
 function createcard(){
   return inventory.forEach((invent)=> DOMSelectors.app.insertAdjacentHTML(
     "afterbegin", 
@@ -22,6 +44,20 @@ function createcard(){
     </div>`) )
   
 }
-DOMSelectors.app.insertAdjacentHTML(
-      "afterbegin", createcard()
-  )    
+
+// if (DOMSelectors.checkbox === true){
+//   createcheapcard();
+// }
+// else{
+//   createcard();
+// }
+DOMSelectors.checkbox.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (DOMSelectors.checkbox.checked === true){
+    createcheapcard();
+  }
+  else{
+    createcard();
+  }
+  
+});
